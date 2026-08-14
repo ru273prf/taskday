b66a224e09e9eddeca9c8cd2325266f3db39039a
ru273prf <54df4zhh27@privaterelay.appleid.com>
2026-08-14 19:43:56 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
-const CACHE="taskday-v2";
+const CACHE="taskday-v3";
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!=="taskday-v3").map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
